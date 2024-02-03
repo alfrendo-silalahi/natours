0ddd3a81713fe945c155edbc281c7b9fe0a31c45
Alfrendo Silalahi <alfrendos72@gmail.com>
2024-02-03 15:31:30 +0700

    add tours resource

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server side!' });
-});
+const getAllTours = (req, res) => {
+  res.status(200).json({ data: 'Get all tours' });
+};
 
-app.post('/', (req, res) => {
-  res.status(201).json({ message: 'POST to this endpoint!' });
-});
+const createTour = (req, res) => {
+  res.status(201).json({ requestBody: req.body });
+};
+
+const getTour = (req, res) => {
+  console.log(req.params);
+  res.status(200).json({ data: `Tour with id ${req.params.id}` });
+};
+
+const updateTour = (req, res) => {
+  console.log(req.params);
+  res.status(200).json({ data: `Tour with id ${req.params.id} updated` });
+};
+
+const deleteTour = (req, res) => {
+  res.status(200).json({ data: `Tour with id ${req.params.id} deleted` });
+};
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+/*
+app.get('/api/v1/tours', getAllTours);
+app.post('/api/v1/tours', createTour);
+app.get('/api/v1/tours/:id', getTour);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
+*/
 
 const port = 3000;
 const host = 'http://localhost';
