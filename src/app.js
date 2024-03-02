f98e8c0a3aa27d7c49fa5a5328cd6aa7fdee038f
Alfrendo Silalahi <alfrendos72@gmail.com>
2024-03-02 21:45:32 +0700

    Add environment variables

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+
 import express from 'express';
 import morgan from 'morgan';
 
@@ -8,8 +11,10 @@ const app = express();
 
 // 1) Middlewares
 app.use(express.json());
-app.use(morgan('dev'));
 app.use(express.static('./public'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // 2) Routes
 app.use('/api/v1/tours', tourRouter);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+
 import app from './app.js';
 
-const port = 3000;
-const host = 'http://localhost';
+const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`App running on ${host}:${port} ...`);
+  console.log(`App running on port ${port} ...`);
 });
