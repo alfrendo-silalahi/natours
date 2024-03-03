1cf8f39bfc6f8671e685b47e8680d859af638aa8
Alfrendo Silalahi <alfrendos72@gmail.com>
2024-03-03 11:10:34 +0700

    Connect with MongoDB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env' });
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successfully!'))
+  .catch((err) => console.log(`DB connection error:  ${err}`));
 
 import app from './app.js';
 
