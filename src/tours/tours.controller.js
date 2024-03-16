b87b5726dcfa5d50577fd054d4bf8ef9df1f1afe
Alfrendo Silalahi <alfrendos72@gmail.com>
2024-03-16 09:56:02 +0700

    create advanced filtering

--- src/tours/tours.controller.js
+++ src/tours/tours.controller.js
@@ -14,12 +14,17 @@ export const getAllTours = async (req, res) => {
     //   .equals(req.query.difficulty);
 
     // Build query
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
     // Execute query
-    const query = Tour.find(queryObj);
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
     const tours = await query;
 
     // Send response
