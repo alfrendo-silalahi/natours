413f3443caf1c54fa7986c8fdfd1047c1a3c153a
Alfrendo Silalahi <alfrendos72@gmail.com>
2024-05-18 10:29:53 +0700

    add swagerr ui & open api

--- src/app.js
+++ src/app.js
@@ -4,13 +4,37 @@ dotenv.config({ path: './.env' });
 
 import express from 'express';
 import morgan from 'morgan';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 import tourRouter from './tours/tours.route.js';
 import userRouter from './users/users.route.js';
 
 const app = express();
 
+// Swagger definition
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '0.0.1',
+      description: 'API Documentation for Express.js application',
+    },
+    servers: [
+      {
+        url: `http://localhost:8000`,
+        description: 'Development server',
+      },
+    ],
+  },
+  apis: ['./src/**/*.route.js'], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
 // 1) Middlewares
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json());
 app.use(express.static('./public'));
 if (process.env.NODE_ENV === 'development') {
--- src/tours/tours.route.js
+++ src/tours/tours.route.js
@@ -10,12 +10,64 @@ const tourRouter = express.Router();
 
 tourRouter
   .route('/')
+  /**
+   * @swagger
+   * /api/v1/tours:
+   *   get:
+   *     summary: Retrieve a list of tours
+   *     responses:
+   *       200:
+   *         description: A list of tours
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: array
+   *               items:
+   *                 type: object
+   */
   .get(tourController.getAllTours)
+  /**
+   * @swagger
+   * /api/v1/users:
+   *   post:
+   *     summary: Retrieve a list of users
+   *     responses:
+   *       200:
+   *         description: A list of users
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: array
+   *               items:
+   *                 type: object
+   */
   .post(tourController.createTour);
 // .post(tourMiddleware.checkReqBody, tourController.createTour);
 
 tourRouter
   .route('/:id')
+  /**
+   * @swagger
+   * /api/v1/tours/{id}:
+   *   get:
+   *     summary: Retrieve tour by id
+   *     parameters:
+   *       - in: path
+   *         name: id
+   *         required: true
+   *         schema:
+   *           type: string
+   *         description: The user id
+   *     responses:
+   *       200:
+   *         description: A tour
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: array
+   *               items:
+   *                 type: object
+   */
   .get(tourController.getTour)
   .patch(tourController.updateTour)
   .delete(tourController.deleteTour);
