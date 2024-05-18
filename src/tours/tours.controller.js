5a75abe1bc25cd018bcb98cef936983db9cf0aeb
Alfrendo Silalahi <alfrendos72@gmail.com>
2024-05-18 09:08:30 +0700

    add fields limiting & pagination

--- src/tours/tours.controller.js
+++ src/tours/tours.controller.js
@@ -33,6 +33,26 @@ export const getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 3) Fields limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    // EXECUTE QUERY
     const tours = await query;
 
     // Send response
