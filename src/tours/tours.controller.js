1972a3d30be06df2a72c7daed04a8cc709f5442e
Alfrendo Silalahi <alfrendos72@gmail.com>
2024-05-23 14:53:48 +0700

    create aliassing for API top 5 cheap tours

--- src/tours/tours.controller.js
+++ src/tours/tours.controller.js
@@ -1,5 +1,12 @@
 import Tour from './tours.model.js';
 
+export const aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price, ratingsAverage,summary,difficulty';
+  next();
+};
+
 export const getAllTours = async (req, res) => {
   try {
     // const tours = await Tour.find({
--- src/tours/tours.route.js
+++ src/tours/tours.route.js
@@ -9,6 +9,10 @@ const tourRouter = express.Router();
 // tourRouter.param('id', tourMiddleware.checkParamId);
 
 tourRouter
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+tourRouter
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
