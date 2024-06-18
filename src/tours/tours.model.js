f9575d3e9cfadc278fc107a2fdfbf86a58682ab3
alfrendo-silalahi <alfrendos72@gmail.com>
2024-06-18 17:59:43 +0700

    creaate tour stat and monthly plan api

--- src/tours/tours.model.js
+++ src/tours/tours.model.js
@@ -23,7 +23,7 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     default: 4.5,
   },
-  ratingQuantity: {
+  ratingsQuantity: {
     type: Number,
     default: 0,
   },
--- src/tours/tours.route.js
+++ src/tours/tours.route.js
@@ -12,6 +12,9 @@ tourRouter
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+tourRouter.route('/tour-stats').get(tourController.getTourStats);
+tourRouter.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 tourRouter
   .route('/')
   .get(tourController.getAllTours)
