e62241ecdfe19c4a03ee9bf5bde24e0acc9af226
Alfrendo Silalahi <alfrendos72@gmail.com>
2025-05-24 12:16:34 +0700

    create new api endpoint update me

--- service/src/users/users.controller.js
+++ service/src/users/users.controller.js
@@ -1,4 +1,5 @@
 import catchAsync from '../utils/catch-async.js';
+import CustomError from '../utils/error.js';
 import User from './users.model.js';
 
 export const getAllUsers = catchAsync(async (req, res, next) => {
@@ -12,3 +13,34 @@ export const getAllUsers = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+export const updateMe = catchAsync(async (req, res, next) => {
+  // 1) create error if user POST password data
+  if (req.body.password || req.body.passwordConfirm)
+    throw new CustomError(
+      'this route is not for password update, please use /update-password',
+      400,
+    );
+
+  // 2) update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
--- service/src/users/users.route.js
+++ service/src/users/users.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import * as userController from './users.controller.js';
+import { protect } from '../middlewares/auth.middleware.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/', userController.getAllUsers);
+userRouter.patch('/update-me', protect, userController.updateMe);
 
 export default userRouter;
