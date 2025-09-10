8eb1a37f16b51cb33a44e20dfa0d7af8ea3e9186
Alfrendo Silalahi <alfrendos72@gmail.com>
2025-09-10 09:16:40 +0700

    fix: error message if redis not available

--- service/src/config/redis.config.js
+++ service/src/config/redis.config.js
@@ -1,11 +1,24 @@
 import { createClient } from 'redis';
 import log from '../utils/logger.js';
 
+const { NODE_ENV } = process.env;
+
 const redisClient = createClient();
 
-redisClient.on('error', (err) => log.error(err.message));
+redisClient.on('error', (err) => {
+  if (err instanceof AggregateError) {
+    log.error('Redis AggregateError occurred');
+    err.errors.forEach((e) => {
+      log.error(e.code);
+    });
+  }
+  if (NODE_ENV === 'prod') process.exit(1);
+});
+
+redisClient.on('reconnecting', () => log.info('Redis reconnecting...'));
+
+redisClient.on('connect', () => log.info('Redis connected successfully'));
 
 await redisClient.connect();
-log.info('Redis connected successfully');
 
 export default redisClient;
